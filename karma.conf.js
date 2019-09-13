8d4edd2cd8e53b0fb872727c615fcf4b8b73d60a
William Almeida <will.allmeida@gmail.com>
2019-09-13 02:06:45 -0300

    Arruma travis

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless', 'Chrome'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
@@ -37,7 +37,7 @@ module.exports = function (config) {
         ]
       }
     },
-    singleRun: true,
+    singleRun: false,
     restartOnFileChange: true
   });
 };
