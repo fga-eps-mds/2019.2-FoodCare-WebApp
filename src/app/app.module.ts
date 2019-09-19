1238bebeb593cceffa2b780e9d8b6566199279ec
Rafaella Junqueira <rafaellafjunqueira@gmail.com>
2019-09-19 00:09:12 -0300

    Adiciona Angular Material ao projeto
    Co-authored-by: geraldovictor <geraldovictor@outlook.com>
    Co-authored-by: LucasPLopes <l.lopes.fga@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
