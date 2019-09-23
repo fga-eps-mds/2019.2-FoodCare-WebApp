5c342131bb847e36acb9299d8160d8e343564ebe
Geraldo Victor <geraldovictor@outlook.com>
2019-09-23 19:42:01 -0300

    Adiciona componente menu-nav
    Co-authored-by: LucasPLopes <l.lopes.fga@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
