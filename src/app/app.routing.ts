976ef0ba92ed04a95bc362ddd172c7075cb7f46d
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-29 21:02:06 -0300

    Atualiza componente nav-menu
    Co-authored-by: RafaellaJunqueira <rafaellafjunqueira@gmail.com>

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
-import { MenuNavComponent } from './menu-nav/menu-nav.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
