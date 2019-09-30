046d76a5d972edaebee3d0cab1cd6e6ebcc1e5e2
Geraldo Victor <geraldovictor@outlook.com>
2019-09-30 11:56:28 -0300

    Atualiza testes do app.component.spec.ts e menu-nav.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -6,6 +7,9 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        RouterTestingModule
       ],
     }).compileComponents();
   }));
