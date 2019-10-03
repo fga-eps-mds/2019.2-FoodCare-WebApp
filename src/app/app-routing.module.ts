23133b690ccc6dd6fa8fc95f696b33db9696fab5
Geraldo Victor <geraldovictor@outlook.com>
2019-10-03 16:11:28 -0300

    Importa rota adiciona alimentos e jogaparte de adicionar do layout da lista de eventos para ela

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { MenuNavComponent } from './menu-nav/menu-nav.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { AdicionaAlimentoComponent } from './adiciona-alimento/adiciona-alimento.component';
 
 const routes: Routes = [
   { path: 'home', component: MenuNavComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'eventos', component: EventosComponent },
+  { path: 'adiciona-alimento', component: AdicionaAlimentoComponent},
 //   Sempre que criar um componente, adicioná-lo aqui
   { path: '',
     redirectTo: '/home',
