b56c360d558efbaf03ec9cb308b401d01d5e0cad
GiovannaBot <gibottino@gmail.com>
2019-10-04 12:27:07 -0300

    Corrige escolha da unidade de medida

--- src/app/adiciona-alimento/adiciona-alimento.component.ts
+++ src/app/adiciona-alimento/adiciona-alimento.component.ts
@@ -10,41 +10,39 @@ import { Alimento } from './adiciona-alimento.interface';
 
 export class AdicionaAlimentoComponent implements OnInit {
 
-  items: Alimento[];
+  alimento: Alimento[];
   error: any;
+  itemUnMedida: string = '';
+  UnMedida: any=['Unidade', 'Kg', 'L'];
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
    this.api.getAlimento().subscribe(
-     (items: Alimento[]) => this.items = items,
+     (alimento: Alimento[]) => this.alimento = alimento,
      (error: any) => this.error = error
    );
  }
 
  delAlimento(id: number) {
    this.api.deletaAlimento(id).subscribe(
-     (success: any) => this.items.splice(
-       this.items.findIndex(item => item.id === id)
+     (success: any) => this.alimento.splice(
+       this.alimento.findIndex(item => item.id === id)
      )
    );
  }
  adAlimento(itemNome: any, itemQuantidade: any, itemCategoria: any, itemUnMedida:any, itemEvento:any) {
    this.api.adicionaAlimento(itemNome, itemQuantidade, itemCategoria, itemUnMedida, itemEvento).subscribe(
-     (item: Alimento) => this.items.push(item)
+     (item: Alimento) => this.alimento.push(item)
    );
  }
 
  editAlimento(id: number, itemNome: any, itemQuantidade: any, itemCategoria: any, itemUnMedida: any, itemEvento: any){
    this.api.editaAlimento(id, itemNome, itemQuantidade, itemCategoria, itemUnMedida, itemEvento).subscribe(
-     (success: any) => this.items.splice(
-       this.items.findIndex(item => item.id === id)
+     (success: any) => this.alimento.splice(
+       this.alimento.findIndex(item => item.id === id)
      )
    );
  }
-
- radioChangeHandler (event: any){
-   this.itemUnMedida = event.target.value;
- }
-
+ 
 }
