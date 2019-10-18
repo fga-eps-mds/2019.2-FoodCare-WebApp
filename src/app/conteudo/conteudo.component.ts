25738408e79a92580da2ad7166ecafce4d536f04
roginaldosemog <roginaldosemog@gmail.com>
2019-10-18 00:07:59 -0300

    Corrige local da função isHandset

--- src/app/conteudo/conteudo.component.ts
+++ src/app/conteudo/conteudo.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'Conteudo',
@@ -13,14 +10,7 @@ export class ConteudoComponent implements OnInit {
 
   private apiRoot = 'http://localhost:8000/';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
   constructor(
-    private breakpointObserver: BreakpointObserver,
     private http: HttpClient
   ) { }
 
