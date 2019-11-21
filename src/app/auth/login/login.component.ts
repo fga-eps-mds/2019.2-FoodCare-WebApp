77a8cced5167a374947940a320532a823ef2a0fc
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2019-11-21 17:50:16 -0300

    Corrige

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'Login',
@@ -9,13 +10,22 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
   error: any;
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private router: Router,
+    private formBuilder: FormBuilder,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      usuario: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
 
   login(username: string, password: string) {
     this.authService.login(username, password).subscribe(
@@ -23,7 +33,13 @@ export class LoginComponent implements OnInit {
         console.log(success);
         this.router.navigate(['eventos-doador']);
       },
-      error => this.error = error
+      error => {
+        this.error = error;
+        console.log(error);
+        alert("Usuário não encontrado");
+      }
     );
   }
+
+
 }
